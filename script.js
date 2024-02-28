605e4048ed663f8fd39f526a172cf7bceb831b3b
aluno <aluno@fatec.campinas.br>
2024-02-27 22:08:40 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const btLogin = document.getElementById("btLogin");
+const btCreateNewAccount = document.getElementById("btCreateNewAccount");
+
+btLogin.onclick = () => {
+    window.location.href = "login"
+}
+
+btLogin.onclick = () => {
+    window.location.href = "criar-nova-conta"
+}
