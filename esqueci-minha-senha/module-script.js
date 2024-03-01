96a6266f5bfb421cf93c57fef9c74b5e5c0e031f
Guilherme Sartori <guilherme.sartori@orys.com.br>
2024-03-01 01:12:11 -0300

    feat forget password

---
+++ esqueci-minha-senha/module-script.js
@@ -0,0 +1,22 @@
+import { getAuth, sendPasswordResetEmail } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js'
+import { showDangerToast, showSuccessToast } from '../assets/js/toast.js';
+
+
+const formForgotPassword = document.getElementById('formForgotPassword');
+
+formForgotPassword.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const email = formForgotPassword.email.value;
+  
+  const auth = getAuth();
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+    showSuccessToast("Email de recuperação de senha enviado com sucesso. Verifique sua caixa de entrada.")
+    formForgotPassword.email.value = "";
+  })
+  .catch((error) => {
+    console.error(error)
+    showDangerToast("Ocorreu um erro ao enviar o email de recuperação de senha. Verifique se o email está correto e tente novamente.")
+  });
+})
